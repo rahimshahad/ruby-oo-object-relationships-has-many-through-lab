49c937443c612a6e801aee43b7c5b5c46a9e7405
rahimshahad <rahim.shahad2@gmail.com>
2021-01-15 17:59:48 -0500

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,21 @@
+class Artist
+    @@all = []
+    attr_accessor :name
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+    def self.all
+        @@all
+    end
+    def songs
+        Song.all.select {|song| song.artist == self}
+    end
+    def new_song(name, genre)
+        song = Song.new(name, self, genre)
+    end
+    def genres
+        songs.map {|song| song.genre}
+    end
 
+end
