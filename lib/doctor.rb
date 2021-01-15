04a931dace5ede50197b8931f5775dbe56c4673e
rahimshahad <rahim.shahad2@gmail.com>
2021-01-15 17:59:42 -0500

    commit tree 70435ba8e96dcd5f06a106b74773be540a67fb39

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,1 +1,20 @@
-
+class Doctor
+    @@all = []
+    attr_accessor :name
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+    def self.all
+        @@all
+    end
+    def appointments
+        Appointment.all.select {|appointment| appointment.doctor == self}
+    end
+    def new_appointment(date, patient)
+        appt = Appointment.new(date, patient, self)
+    end
+    def patients
+        appointments.map {|appointment| appointment.patient}
+    end
+end
